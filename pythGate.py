6ec16726092513ee0ca4768fce940ccda61f8bce
Michael Salaverry <barakplasma@gmail.com>
2009-05-29 16:06:12 -0700

    viewer.sh is the viewer I always wanted. updates every x seconds for xxxx station

--- pythGate.py
+++ pythGate.py
@@ -1,4 +1,4 @@
-import getpass, imaplib
+import imaplib
 
 M = imaplib.IMAP4_SSL('imap.gmail.com', 993)
 userName = "nxtBart"
@@ -8,8 +8,14 @@ print "logged in"
 M.select()
 typ, data = M.search(None, 'ALL')
 print "printing msg's now"
+allmsgs = ""
+counter = 1
 for num in data[0].split():
     typ, data = M.fetch(num, '(RFC822)')
-    print 'Message %s\n%s\n' % (num, data[0][1])
+    #print 'Message %s\n%s\n' % (num, data[0][1])
+    while(counter > 0):
+        allmsgs += 'Message %s\n%s\n' % (num, data[0][1])
+#	counter = counter - 1
+print allmsgs
 M.close()
 M.logout()
---
+++ viewer.sh
@@ -0,0 +1,4 @@
+echo "started script"
+python viewArrivals.py
+sleep 30
+sh viewer.sh
