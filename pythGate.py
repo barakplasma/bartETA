27f42814f5cfcee5cd7c3d7dbafb29b13b55a654
Michael Salaverry <barakplasma@gmail.com>
2009-06-04 14:38:23 -0700

    fuxing with pythgate

--- pythGate.py
+++ pythGate.py
@@ -10,15 +10,11 @@ M.select()
 typ, data = M.search(None, 'ALL')
 #print "printing msg's now"
 allmsgs = ""
-counter = 1
+#counter = 0
 for num in data[0].split():
     typ, data = M.fetch(num, '(RFC822)')
-    #print 'Message %s\n%s\n' % (num, data[0][1])
+    print 'Message %s\n%s\n' % (num, data[0][1])
     allmsgs += 'Message %s\n%s\n' % (num, data[0][1])
-    counter = counter - 1
-    #while(counter > 0):
-#       allmsgs += 'Message %s\n%s\n' % (num, data[0][1])
-#	counter = counter - 1
 #print allmsgs
 txtmsgs = open('./txtmsgs.txt','r+')
 txtmsgs.write(allmsgs)
